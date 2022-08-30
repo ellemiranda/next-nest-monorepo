8dc033bd17e5d154442a585b3a36a6deb7933b68
Eduardo Miranda <eduardomiranda@ciencias.unam.mx>
2022-08-29 22:37:39 -0500

    fixing workflow set env vars

--- .github/common/preid-tag.sh
+++ .github/common/preid-tag.sh
@@ -5,12 +5,15 @@ if [[ $VERISON =~ [0-9]+.[0-9]+.[0-9]+-(alpha|beta|rc|hotfix|test).[0-9]+$ ]]; t
   exit 1
 else
   echo "Searching for valid version"
-  LAST_VERSION=$(git tag -l "$VERSION-$PREID*" | sort -V | tail -n1)
+  LAST_VERSION=$(git tag -l "v$VERSION-$PREID*" | sort -V | tail -n1)
+  echo "LAST VERSION = $LAST_VERSION"
 fi
 
 if [ -n "$LAST_VERSION" ]; then
   npx semver "$LAST_VERSION"
+  echo $LAST_VERSION
   NEXT_VERSION=$(npx semver -i prerelease "$LAST_VERSION")
+  echo $NEXT_VERSION
 else
   NEXT_VERSION="$VERSION-$PREID.0"
 fi

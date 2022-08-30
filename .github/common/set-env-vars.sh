f275e653f96e134ccded5b63bc4a1379bb57319b
Eduardo Miranda <eduardomiranda@ciencias.unam.mx>
2022-08-29 22:37:39 -0500

    fixing workflow set env vars

--- .github/common/set-env-vars.sh
+++ .github/common/set-env-vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$PREID="latest"
+PREID="latest"
 
 if [ -z "$ROOT" ]; then
   ROOT="."
@@ -12,30 +12,31 @@ echo "AWS_REGION=us-east-1" >> $GITHUB_ENV
 
 if [ -f "lerna.json" ]; then
   echo "Lerna project detected"
+  TYPE='lerna'
 else
   echo "Unknow repository"
   exit 1
 fi
 
 if [ -z $VERSION ]; then
-  if [[ $GITHUB_REF == refs\/tags\/v ]]; then
+  if [[ $GITHUB_REF =~ refs\/tags\/v ]]; then
     echo "Getting version from semver"
     VERSION=$(npx semver "${GITHUB_REF#"refs/tags/v"}")
   elif [ "$TYPE" == 'lerna' ]; then
     echo "Getting version from lerna"
-    VERSION=$(jg -r .version lerna.json)
+    VERSION=$(jq -r .version lerna.json)
   elif [ -f "package.json" ]; then
     echo "Getting version from package.json"
-    VERSION=$(jg -r .version "$ROOT/package.json")
+    VERSION=$(jq -r .version "$ROOT/package.json")
   else
     echo "Failed to get version"
     exit 1
   fi
 fi
 
-DOCKER_REGISTRY=925569971735.dkr.ecr.us-east-1.amazonaws.com
+DOCKER_REGISTRY=925569971735.dkr.ecr.us-east-1.amazonaws.com/ess
 echo "DOCKER_REGISTRY=$DOCKER_REGISTRY" >> $GITHUB_ENV
-echo "DOCKER_TAG=$DOCKER_TAG/$PROJECT:$VERSION" >> $GITHUB_ENV
+echo "DOCKER_TAG=$DOCKER_REGISTRY/$PROJECT:$VERSION" >> $GITHUB_ENV
 echo "ROOT=$ROOT" >> $GITHUB_ENV
 
 echo "REPO_TYPE=$TYPE" >> $GITHUB_ENV

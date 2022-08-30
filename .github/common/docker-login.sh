5a8e561813111bea29d0aeb4536438ed25e88668
Eduardo Miranda <eduardomiranda@ciencias.unam.mx>
2022-08-29 22:37:39 -0500

    fixing workflow set env vars

--- .github/common/docker-login.sh
+++ .github/common/docker-login.sh
@@ -6,8 +6,6 @@ source .github/common/set-env-vars.sh
 
 echo "Login to Docker registry"
 
-aws ecr get-login-password
-  --region us-east-1 |
-  docker login \
+aws ecr get-login-password --region us-east-1 | docker login \
     --username AWS \
     --password-stdin $DOCKER_REGISTRY
